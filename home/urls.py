dfba218edf2151dcdba92f0c80dfdf8045509ee3
Ernesto Pena <ernestopenad@gmail.com>
2020-11-07 08:48:47 -0600

    home, blog projects created and home page is running. project css folder/file and img folder wwere also added

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,11 @@
-from django.contrib import admin
+from django.contrib import admin 
 from django.urls import path, include
-from home import views
+from home.views import home, about, resume, portfolio, contact
 
 urlpatterns = [
-    path('' , views.homePage, name='home'),
+    path('', home , name = 'home'),
+    path('about', about , name = 'about'),
+    path('resume', resume , name = 'resume'),
+    path('portfolio', portfolio , name = 'portfolio'),
+    path('contact', contact, name = 'contact'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def homePage(request):
-    return HttpResponse('This is a test')
+#Home Page View
+def home(request):
+    return render(request, 'home.html')
+
+#About Page View
+def about(request):
+    return render(request, 'about.html')
+
+#Resume Page View
+def resume(request):
+    return render(request, 'resume.html')
+
+#Portfolio Page View
+def portfolio(request):
+    return render(request, 'portfolio.html')
+
+#Contact Page View
+def contact(request):
+    return render(request, 'contact.html')
